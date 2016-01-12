8863f18263e9ad789bb9c02b4c886a9d40fec996
lizhi Lin <lin@linlizhidemacbook-pro.local>
2016-01-12 20:49:04 +0800

    1-12  home

---
+++ StoreCheck/Others/SelfDefine/TabbarCOntroller/LLZTabBarButton.h
@@ -0,0 +1,13 @@
+//
+//  LLZTabbarButton.h
+//  StoreCheck
+//
+//  Created by skunk  on 15/11/12.
+//  Copyright (c) 2015年 linlizhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLZTabBarButton : UIButton
+@property (nonatomic, strong) UITabBarItem * item;
+@end
